40f2e3d5345d4fa2ae85e8def6fce4942c4dfb90
pablowinck <pablowinck123@gmail.com>
2021-08-12 15:35:33 -0300

    Melhoria de Código + Cores nos prints

--- backup.sh
+++ backup.sh
@@ -1,30 +1,37 @@
 ################################
 # SCRIPT BACKUP SERVIDOR DADOS #
 ################################
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+DATA=$(date +%d-%m-%Y-%H.%M)
+
+#! PARTIÇÃO ONDE O HD EXTERNO É MONTADO
+PARTICAO=/dev/sda3
+###* sempre que trocar a porta USB, o caminho será o diferente
 
 function realizarBackup {
-    # COMPACTA TODO O CONTEÚDO DAS PASTAS DENTRO DE /BACKUP INDIVIDUALMENTE.
+    #* COMPACTA TODO O CONTEÚDO DAS PASTAS DENTRO DE /BACKUP INDIVIDUALMENTE.
     tar -zcvf /backup/backup-"$DATA".tar.gz /home/Grupo/
 
-    # DESMONTA O PONTO DE MONTAGEM /BACKUP
+    #* DESMONTA O PONTO DE MONTAGEM /BACKUP
     umount /backup
     #umount /dev/sda3
 
-    echo "Backup realizado com sucesso"
+    printf "${GREEN}Backup realizado com sucesso${NC}\n"
 
     exit 1
 }
 
-DATA=$(date +%d-%m-%Y-%H.%M)
-
-# MONTA O PONTO DE MONTAGEM /BACKUP
-mount /dev/sda3 /backup
+#* MONTA O PONTO DE MONTAGEM /BACKUP
+mount $PARTICAO /backup
 montado=$(mount | grep /backup)
 
 if [ -e /backup/backup-"$DATA".tar.gz ]; then
     t=1
     while [ $t == 1 ]; do
-        echo "Esse backup já existe, deseja sobrescreve-lo? [ s / n ]"
+        printf "Esse backup já existe, deseja sobrescreve-lo? [ s / n ]  "
         read sim_nao
         case $sim_nao in
         s) realizarBackup ;;
@@ -32,13 +39,13 @@ if [ -e /backup/backup-"$DATA".tar.gz ]; then
             echo "Script Finalizado"
             t=$(($t + 1))
             ;;
-        *) echo "Opção inválida" ;;
+        *) printf "${RED}Opção inválida${NC} \n" ;;
         esac
     done
 
 else
 
-    # SE A MONTAGEM NÃO ESTIVER UP ENTÃO FECHA, CASO CONTRÁRIO REALIZA O BACKUP
+    #! SE A MONTAGEM NÃO ESTIVER UP ENTÃO FECHA, CASO CONTRÁRIO REALIZA O BACKUP
     if [ -z "$montado" ]; then
         exit 1
     else
